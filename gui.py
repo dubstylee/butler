5c29a6e7b8e0816dbc2440873a41e000a56846ba
Brian Williams <williamsbm2000@comcast.net>
2017-11-04 17:24:48 -0700

    skeleton for top/bottom frame

--- gui.py
+++ gui.py
@@ -1,3 +1,31 @@
 import Tkinter as tk
+from shared import exit_program, mqtt_client, mqtt_topic
 
-// placeholder for GUI
+
+def part_a(gui):
+    # part A Abhishek assert
+    print("Abhi assert")
+
+
+def part_b(gui):
+    # part B Brian property
+    print("Brian property")
+
+
+# placeholder for GUI
+def main():
+    root = tk.Tk()
+    top_frame = tk.Frame(root)
+    top_frame.pack()
+
+    bottom_frame = tk.Frame(root)
+    bottom_frame.pack(side=tk.BOTTOM)
+
+    part_a(top_frame)
+    part_b(bottom_frame)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
