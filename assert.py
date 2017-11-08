281f0ec6bdb4f2d5171517ef76349c08758ecf59
Abhishek Yenpure <abhishek@uoregon.edu>
2017-11-07 16:48:18 -0800

    Keeping only the essential alphabet in the assert
    - Trying to model the assert closely to the FSM for the assert
    - State changes defined in the ENUM

--- assert.py
+++ assert.py
@@ -16,23 +16,17 @@ from shared import *
 #  philosopher, or to have a common assert for all
 #  philosophers.
 
-#  1 : assert is valid, 0 : assert is invalid
-#  Starting out with all 1s because of the weak until
-assertStatus = [1, 1, 1, 1, 1]
+class Status(Enum) :
+  ERROR = -1
+  BEGIN = 0
+  HOMEFREE = 1
 
-#  1 means the philosopher is at the table
-philosopherStatus = [0, 0, 0, 0, 0]
-
-def checkAssert(identifier)
-  #  Perform validations over here
-  if philosopherStatus[identifier] == 0
-    #  Means the philosopher is not at the table and is 
-    #  eating, in which case the assert is valid.
-    assertStatus[identifier] = 1
-  elif philosopherStatus[identifier] == 1
-    #  Means the philosopher is at the table and is eating,
-    #  in which case the assert is invalid.
-    assertStatus[identifier] = 0
+#  Since we have a weak until in the property, we have
+#  a valid BEGIN state where the assert passes for all
+#  philosophers
+assertStatus = [Status.BEGIN, Status.BEGIN, 
+                Status.BEGIN, Status.BEGIN, 
+                Status.BEGIN]
 
 def on_message(client, userdata, msg):
   global COUNT
@@ -40,14 +34,13 @@ def on_message(client, userdata, msg):
   split = message.split(' ')
   action = split[3]
   identifier = int(split[4])
-  if "SITDOWN" == action :
-    philosopherStatus[identifier] = 1
-    send_message("UPDATEA %s" %message)
-  elif "EATING" == action :
-    checkAssert(identifier)
+  if "EATING" == action :
+    if assertStatus[identifier] == Status.BEGIN
+      assertStatus[identifier] = Status.ERROR
     send_message("UPDATEA %s" %message)
   elif "ARISE" == action :
-    philosopherStatus[identifier] = 0
+    if assertStatus[identifier] == Status.BEGIN
+      status = Status.HOMEFREE
     send_message("UPDATEA %s" %message)
 
 def main():
@@ -57,7 +50,7 @@ def main():
   while True:
     time.sleep(1)
     #Check assert status and control the LED accordingly
-    if 0 in assertStatus
+    if Status.ERROR in assertStatus
       sendmessage("UPDATEA ASSERTFAILED")
 
 if __name__ == "__main__": main()  
