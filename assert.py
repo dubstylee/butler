5d759e6f8ce126e4f7e8ecc0799756e4a7c1fe85
Abhishek Yenpure <abhishek@uoregon.edu>
2017-11-08 15:06:00 -0800

    Adding changes for assert to work with any configuration

--- assert.py
+++ assert.py
@@ -12,10 +12,8 @@
 from enum import Enum
 from shared import *
 
-#  These arrays gives a way to maintain an assert per
-#  philosopher, or to have a common assert for all
-#  philosophers.
-
+# Enum to track the state of the system according to
+# LTSA developed for the system.
 class Status(Enum) :
   ERROR = -1
   BEGIN = 0
@@ -24,37 +22,48 @@ class Status(Enum) :
 #  Since we have a weak until in the property, we have
 #  a valid BEGIN state where the assert passes for all
 #  philosophers
-system = Status.HOMEFREE
-assertStatus = [Status.BEGIN, Status.BEGIN, 
-                Status.BEGIN, Status.BEGIN, 
-                Status.BEGIN]
+assertStatus = Status.BEGIN
+
+eatIdentifier = 0
+ariseIdentifier = 0
 
 def on_message(client, userdata, msg):
-  global COUNT
+  global assertStatus
   message = msg.payload
+  print message
+  if (not ("EATING" in message or "ARISE" in message)) or ("UPDATEA" in message):
+    return
   split = message.split(' ')
-  action = split[3] 
-  if "EATING" == action :
-    identifier = int(split[4])
-    if assertStatus[identifier] == Status.BEGIN:
-      assertStatus[identifier] = Status.ERROR
+  action = split[3]
+  identifier = int(split[4])
+
+  if "EATING" == action and identifier == eatIdentifier:   
+    if assertStatus == Status.BEGIN:
+      assertStatus = Status.ERROR
+      send_message("UPDATEA ASSERTFAILED")
     send_message("UPDATEA %s" %message)
-  elif "ARISE" == action :
-    identifier = int(split[4])
-    if assertStatus[identifier] == Status.BEGIN:
-      assertStatus[identifier] = Status.HOMEFREE
+
+  elif "ARISE" == action and identifier == ariseIdentifier:
+    if assertStatus == Status.BEGIN:
+      assertStatus = Status.HOMEFREE
     send_message("UPDATEA %s" %message)
 
 def main():
-  global system
+  global assertStatus
+  global eatIdentifier
+  global ariseIdentifier
+
+  if len(sys.argv) < 3:
+    print "Wrong number of arguments : assert.py <eat identifier> <arise identifier>"
+    exit_program()
+
+  eatIdentifier = int(sys.argv[1])
+  ariseIdentifier = int(sys.argv[2])
+
   mqtt_client.on_message = on_message
   mqtt_client.will_set(mqtt_topic, "Will of Validator %s\n\n", 0, False)
-  mqtt_client.loop_start()  
+  mqtt_client.loop_start() 
   while True:
-    time.sleep(1)
-    #Check assert status and control the LED accordingly
-    if Status.ERROR in assertStatus and system == Status.HOMEFREE :
-      system = Status.ERROR
-      send_message("UPDATEA ASSERTFAILED")
+    time.sleep(1) 
 
 if __name__ == "__main__": main()  
